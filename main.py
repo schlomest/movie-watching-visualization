22a11d2b802dd26f33ae4cc574b4b1493c43f076
Daniel <djschlomer@gmail.com>
2020-01-11 13:35:15 -0600

    created basic bar chart for movies per year

--- main.py
+++ main.py
@@ -1,22 +1,41 @@
+from os import listdir
 from typing import List
 
-class Movie():
-    def __init__(self, name, date_watched=None, rating=None):
-        self.name = name
-        self.date_watched = date_watched
-        self.rating = rating
+import matplotlib.pyplot as pyplot
+import numpy
 
-    def __repr__(self):
-        return self.name
+# class Movie():
+#     def __init__(self, name, date_watched=None, rating=None):
+#         self.name = name
+#         self.date_watched = date_watched
+#         self.rating = rating
 
-MovieList = List[Movie]
+#     def __repr__(self):
+#         return self.name
 
-movie_list = []
+# MovieList = List[Movie]
 
-with open('data/2015.txt') as file:
-    for line in file:
-        movie = Movie(line.strip())
-        print(movie)
-        movie_list.append(movie)
+# movie_list = []
 
-print(movie_list)
+# with open('data/2015.txt') as file:
+#     for line in file:
+#         movie = Movie(line.strip())
+#         movie_list.append(movie)
+
+
+# get years
+years = [int(year.rstrip('.tx')) for year in listdir('data')]
+years.sort()
+
+movies_watched = []
+for year in years:
+    total = 0
+    with open('data/{0}.txt'.format(year)) as file:
+        for line in file:
+            total += 1
+    movies_watched.append(total)
+
+
+pyplot.bar(years, movies_watched, width=.6)
+pyplot.title("Movies Watched")
+pyplot.show()
