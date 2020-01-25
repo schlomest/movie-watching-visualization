97cdd77ef1a69c81f68b82411ace211c6e2da538
Daniel <djschlomer@gmail.com>
2020-01-25 13:35:10 -0600

    refactored data transformation code

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as pyplot
 import numpy
 
 from extract import load_movie_data
+from transform import get_ratings_totals, get_weekday_totals, get_yearly_totals
 
 
 def autolabel(rects, ax):
@@ -19,8 +20,7 @@ def autolabel(rects, ax):
 movie_data = load_movie_data()
 
 # MOVIES WATCHED PER YEAR BAR CHART
-totals = [len(movie_data[k]) for k in movie_data.keys()]
-
+totals = get_yearly_totals(movie_data)
 fig, ax = pyplot.subplots()
 rects = ax.bar(movie_data.keys(), totals, width=.6)
 ax.set_title('Total Movies Watched')
@@ -29,26 +29,7 @@ fig.tight_layout()
 pyplot.show()
 
 # MOVIES WATCHED PER DAY OF THE WEEK BAR CHART
-movies_by_weekday = {"Sunday": 0, "Monday": 0, "Tuesday": 0,
-                     "Wednesday": 0, "Thursday": 0, "Friday": 0, "Saturday": 0}
-for year in movie_data.keys():
-    if year >= 2017:
-        for movie in movie_data[year]:
-            if movie.date_watched.weekday() == 6:
-                movies_by_weekday["Sunday"] += 1
-            elif movie.date_watched.weekday() == 0:
-                movies_by_weekday["Monday"] += 1
-            elif movie.date_watched.weekday() == 1:
-                movies_by_weekday["Tuesday"] += 1
-            elif movie.date_watched.weekday() == 2:
-                movies_by_weekday["Wednesday"] += 1
-            elif movie.date_watched.weekday() == 3:
-                movies_by_weekday["Thursday"] += 1
-            elif movie.date_watched.weekday() == 4:
-                movies_by_weekday["Friday"] += 1
-            elif movie.date_watched.weekday() == 5:
-                movies_by_weekday["Saturday"] += 1
-
+movies_by_weekday = get_weekday_totals(movie_data)
 fig, ax = pyplot.subplots()
 rects = ax.bar(movies_by_weekday.keys(),
                movies_by_weekday.values(), width=.6, color='g')
@@ -59,13 +40,7 @@ pyplot.show()
 
 
 # RATINGS PIE CHART
-ratings = {1: 0, 2: 0, 3: 0, 4: 0, 5: 0}
-for year in movie_data.keys():
-    if year >= 2018:
-        for movie in movie_data[year]:
-            if movie.rating:
-                ratings[movie.rating] += 1
-
+ratings = get_ratings_totals(movie_data)
 # Currently there are 0 'hated' ratings, so I removed that data from the vizualization
 labels = "Didn't Like", 'Meh', 'Liked', 'Loved'
 sizes = list(ratings.values())[1:]
---
+++ transform.py
@@ -0,0 +1,39 @@
+def get_yearly_totals(movie_data):
+    """y data for movies watched per year bar chart"""
+    return [len(movie_data[k]) for k in movie_data.keys()]
+
+
+def get_weekday_totals(movie_data):
+    """data for movies watched by weekday bar chart"""
+    movies_by_weekday = {"Sunday": 0, "Monday": 0, "Tuesday": 0,
+                         "Wednesday": 0, "Thursday": 0, "Friday": 0, "Saturday": 0}
+    for year in movie_data.keys():
+        if year >= 2017:
+            for movie in movie_data[year]:
+                if movie.date_watched.weekday() == 6:
+                    movies_by_weekday["Sunday"] += 1
+                elif movie.date_watched.weekday() == 0:
+                    movies_by_weekday["Monday"] += 1
+                elif movie.date_watched.weekday() == 1:
+                    movies_by_weekday["Tuesday"] += 1
+                elif movie.date_watched.weekday() == 2:
+                    movies_by_weekday["Wednesday"] += 1
+                elif movie.date_watched.weekday() == 3:
+                    movies_by_weekday["Thursday"] += 1
+                elif movie.date_watched.weekday() == 4:
+                    movies_by_weekday["Friday"] += 1
+                elif movie.date_watched.weekday() == 5:
+                    movies_by_weekday["Saturday"] += 1
+
+    return movies_by_weekday
+
+
+def get_ratings_totals(movie_data):
+    ratings = {1: 0, 2: 0, 3: 0, 4: 0, 5: 0}
+    for year in movie_data.keys():
+        if year >= 2018:
+            for movie in movie_data[year]:
+                if movie.rating:
+                    ratings[movie.rating] += 1
+
+    return ratings
