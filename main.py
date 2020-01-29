7aa2ce6530465911f9c8876fb2d3fab41325a618
Daniel <djschlomer@gmail.com>
2020-01-29 04:50:47 -0600

    added movies per month graph

--- main.py
+++ main.py
@@ -38,6 +38,47 @@ autolabel(rects, ax)
 fig.tight_layout()
 pyplot.show()
 
+# MOVIES WATCHED PER MONTH
+movie_list = []
+for k, v in movie_data.items():
+    if k >= 2017:
+        movie_list += v
+
+
+# number of movies per month, year
+monthly_data = {}
+for movie in movie_list:
+    coverted_datetime = numpy.datetime64(movie.date_watched.isoformat()[:-3])
+    if coverted_datetime in monthly_data.keys():
+        monthly_data[coverted_datetime] += 1
+    else:
+        monthly_data[coverted_datetime] = 1
+
+
+years = mdates.YearLocator()   # every year
+months = mdates.MonthLocator()  # every month
+years_fmt = mdates.DateFormatter('%Y')
+
+fig, ax = pyplot.subplots()
+ax.plot_date(monthly_data.keys(), monthly_data.values())
+
+# format the ticks
+ax.xaxis.set_major_locator(years)
+ax.xaxis.set_major_formatter(years_fmt)
+ax.xaxis.set_minor_locator(months)
+
+# round to nearest years.
+datemin = numpy.datetime64('2017', 'Y')
+datemax = numpy.datetime64('2020', 'Y')
+ax.set_xlim(datemin, datemax)
+
+# format the coords message box
+ax.format_xdata = mdates.DateFormatter('%Y-%m')
+ax.grid(True)
+fig.autofmt_xdate()
+
+pyplot.show()
+
 
 # RATINGS PIE CHART
 ratings = get_ratings_totals(movie_data)
@@ -46,8 +87,7 @@ labels = "Didn't Like", 'Meh', 'Liked', 'Loved'
 sizes = list(ratings.values())[1:]
 
 fig1, ax1 = pyplot.subplots()
-ax1.pie(sizes, labels=labels, autopct='%1.1f%%',
-        shadow=True, startangle=180)
+ax1.pie(sizes, labels=labels, autopct='%1.1f%%', startangle=180)
 ax1.axis('equal')
 pyplot.show()
 
